5f1c120de4ef2aa6f2ad48194a09108ffdc22bf2
amiralam0 <sharpprogrammer2018@gmail.com>
2023-04-08 02:33:46 +0530

    BuilderEditor

--- client/src/pages/Builder/BuilderEditor.tsx
+++ client/src/pages/Builder/BuilderEditor.tsx
@@ -1,4 +1,14 @@
+import { Table, createStyles, Text } from '@mantine/core';
+
+const useStyles = createStyles(() => ({
+  tableHeader: {
+    fontWeight: 500,
+  },
+}));
+
 function BuilderEditor() {
+  const { classes } = useStyles();
+
   return (
     <div className="flex flex-col p-4 bg-white shadow w-full">
       <h5 className="text-xl text-gray-600    font-bold dark:text-dark">
@@ -6,34 +16,30 @@ function BuilderEditor() {
       </h5>
 
       <div className="relative overflow-x-auto mt-5">
-        <table className=" w-full table-fixed  text-sm text-left text-white-500 dark:text-gray-600 ">
-          
-          <thead className=" text-xs text-white-700 uppercase border dark:bg-white-700 dark:text-dark-700">
+        <Table className='table-fixed'>
+          <thead>
             <tr>
-              <th scope="col" className="px-6 py-3 w-40">
-                Column
+              <th className='w-40'>
+                <Text className={classes.tableHeader}>Column</Text>
               </th>
-              <th scope="col" className="px-6 py-3">
-                API
+              <th>
+                <Text className={classes.tableHeader}>API Endpoint</Text>
               </th>
-              <th scope="col" className="px-6 py-3 w-40">
-                Actions
+
+              <th className='w-40'>
+                <Text className={classes.tableHeader}>Actions</Text>
               </th>
             </tr>
           </thead>
-          <tbody >
-            <tr className="bg-white border-b dark:bg-white-800 ">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark"
-              >
-                Name
-              </th>
-              <td className="px-6 py-4">https://www.google.com/hello</td>
-              <td className="px-6 py-4 whitespace-no-wrap">Delete / Edit</td>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>https://www.google.com</td>
+              <td>Delete / Edit</td>
+
             </tr>
           </tbody>
-        </table>
+        </Table>
       </div>
     </div>
   );
