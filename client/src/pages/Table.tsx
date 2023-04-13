a99850e48ee224d2cd08121e4d0144f2f74d4711
amiralam0 <sharpprogrammer2018@gmail.com>
2023-04-13 22:22:31 +0530

    added mantine data table

--- client/src/pages/Table.tsx
+++ client/src/pages/Table.tsx
@@ -1,140 +1,33 @@
 import { useParams } from 'react-router-dom';
 import Box from '../components/Box';
+import { DataTable } from 'mantine-datatable';
 import { trpc } from '../util/trpc';
+import { Button } from '@mantine/core';
 
 function Table() {
   const { view } = useParams();
-  const {data}  = trpc.table.getTables.useQuery()
-
-console.log(data)
-
+  const { data } = trpc.table.getTables.useQuery();
 
   return (
     <section className="bg-gray-50 white:bg-gray-900 p-3 sm:p-5 h-full">
-      <div className="mx-auto  px-4 ">
-        <div className="bg-white white:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-          <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-            <div className="w-full md:w-1/2">
-              <h5 className="text-xl font-bold dark:text-dark ">
-                {view?.replaceAll('-', ' ')}
-              </h5>
-              <p className="text-sm font-medium text-gray-600 dark:text-dark pb-4">
-             
-              </p>
-              <div className="flex items-center">
-                <label htmlFor="simple-search" className="sr-only">
-                  Search
-                </label>
-                <div className="relative w-full">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-gray-500 white:text-gray-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    type="text"
-                    id="simple-search"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 white:bg-gray-700 white:border-gray-600 white:placeholder-gray-400 white:text-white white:focus:ring-primary-500 white:focus:border-primary-500"
-                    placeholder="Search"
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-              <button
-                type="button"
-                className="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 white:bg-primary-600 white:hover:bg-primary-700 focus:outline-none white:focus:ring-primary-800"
-              >
-                <svg
-                  className="h-3.5 w-3.5 mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                  />
-                </svg>
-                Add product
-              </button>
-              <div className="flex items-center space-x-3 w-full md:w-auto">
-                <button
-                  id="actionsDropdownButton"
-                  data-dropdown-toggle="actionsDropdown"
-                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 white:focus:ring-gray-700 white:bg-gray-800 white:text-gray-400 white:border-gray-600 white:hover:text-white white:hover:bg-gray-700"
-                  type="button"
-                >
-                  <svg
-                    className="-ml-1 mr-1.5 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    />
-                  </svg>
-                  Actions
-                </button>
-                <button
-                  id="filterDropdownButton"
-                  data-dropdown-toggle="filterDropdown"
-                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 white:focus:ring-gray-700 white:bg-gray-800 white:text-gray-400 white:border-gray-600 white:hover:text-white white:hover:bg-gray-700"
-                  type="button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                    className="h-4 w-4 mr-2 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  Filter
-                  <svg
-                    className="-mr-1 ml-1.5 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    />
-                  </svg>
-                </button>
-              
-              </div>
-            </div>
-          </div>
-          <div className="overflow-x-auto" />
+      <div className="bg-white white:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+        <Button color="red" variant="outline">
+          Red
+        </Button>
+        <div className="items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+          <DataTable
+            w={'100%'}
+            columns={[
+              { accessor: 'name' },
+              { accessor: 'streetAddress' },
+              { accessor: 'city' },
+              { accessor: 'state' },
+            ]}
+            records={[]}
+          />
         </div>
+        <div className="overflow-x-auto" />
       </div>
-
     </section>
   );
 }
--- server/routers/table.ts
+++ server/routers/table.ts
@@ -1,8 +1,19 @@
 import { publicProcedure, router } from '../trpc';
 import { prisma } from '../prismaClient';
+import { z } from 'zod';
+
+const addTableSchema = z.object({
+  tableName: z.string(),
+  columns: z.string(),
+});
 
 export const TableRouter = router({
   getTables: publicProcedure.query(() => {
     return prisma.table.findMany();
+  }),
+  addTable: publicProcedure.input(addTableSchema).mutation(({ input }) => {
+    return prisma.table.create({
+      data: input,
+    });
   }),
 });
